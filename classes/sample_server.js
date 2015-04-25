e85c58b78be297dc4f7decb5c4cf2f53b89e3854
Stijn Kuipers <stijnkuipers@gmail.com>
2015-04-25 16:12:24 +0200

    working rpc

--- classes/sample_server.js
+++ classes/sample_server.js
@@ -5,11 +5,10 @@
 
 define(function(require, exports, module)
 {
-	var node = require("../classes/node")
+	var node = require("$CLASSES/node")
 	return node.extend("sample_server", function()
 	{
 		this.attribute("init", "event")
-		this.attribute("load", "method")
 		
 		this.init = function()
 		{
@@ -17,9 +16,9 @@ define(function(require, exports, module)
 			console.color('~br~sample_server~~ object started on server\n')	
 		}
 		
-		this.load = function()
+		this.load = function(param)
 		{
-			console.log("load called!");
+			console.log("load called! incoming param: "  + param);
 			return "payload";
 		}
 	})
